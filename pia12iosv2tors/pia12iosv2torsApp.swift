abc67eba23523150326bee59fc31e786394df2fd
alice <alicelouisew95@gmail.com>
2023-11-09 09:13:17 +0100

    Initial Commit

---
+++ pia12iosv2tors/pia12iosv2torsApp.swift
@@ -0,0 +1,17 @@
+//
+//  pia12iosv2torsApp.swift
+//  pia12iosv2tors
+//
+//  Created by Alice Wheeler on 2023-11-09.
+//
+
+import SwiftUI
+
+@main
+struct pia12iosv2torsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
